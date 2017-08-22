7d0983c274e2f24f6041fb93d61c78003f19f05a
Your Name <you@example.com>
2017-08-22 23:20:19 +0530

    practice questions

---
+++ character_input.py
@@ -0,0 +1,8 @@
+name = input("enter your name : ")
+print(name)
+age = int(input("enter your age : "))
+print(age)
+
+year = (2018 - age + 99 )
+
+print("year you will turn after 100 years will be :  " + str(year))
